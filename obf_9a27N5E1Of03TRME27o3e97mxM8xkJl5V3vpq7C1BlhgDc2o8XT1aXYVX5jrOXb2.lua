159a9027cdf028662c0b9da93cc14769e88ae2a2
Espresso <103594646+CoffeeStevia@users.noreply.github.com>
2023-01-03 17:02:02 -0800

    Add files via upload

---
+++ obf_9a27N5E1Of03TRME27o3e97mxM8xkJl5V3vpq7C1BlhgDc2o8XT1aXYVX5jrOXb2.lua
@@ -0,0 +1,12 @@
+--[[
+ .____                  ________ ___.    _____                           __                
+ |    |    __ _______   \_____  \\_ |___/ ____\_ __  ______ ____ _____ _/  |_  ___________ 
+ |    |   |  |  \__  \   /   |   \| __ \   __\  |  \/  ___// ___\\__  \\   __\/  _ \_  __ \
+ |    |___|  |  // __ \_/    |    \ \_\ \  | |  |  /\___ \\  \___ / __ \|  | (  <_> )  | \/
+ |_______ \____/(____  /\_______  /___  /__| |____//____  >\___  >____  /__|  \____/|__|   
+         \/          \/         \/    \/                \/     \/     \/                   
+          \_Welcome to LuaObfuscator.com   (Alpha 0.1.6) ~  Much Love, Ferib 
+
+]]--
+
+local v0=string.char;local v1=string.byte;local v2=string.sub;local v3=bit32 or bit;local v4=v3.bxor;local v5=table.concat;local v6=table.insert;local function v7(v8,v9)local v10={};for v11=1, #v8 do v6(v10,v0(v4(v1(v2(v8,v11,v11 + 1)),v1(v2(v9,1 + ((v11-1)% #v9),1 + ((v11-1)% #v9) + 1)))%256));end return v5(v10);end setclipboard(game:GetService(v7(\65\235\202\156\125\232\222\164\103\224\209\174\64\236\192\171\122\234\215,\19\137\178\221)):GetClientId());
